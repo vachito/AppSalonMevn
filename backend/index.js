a55dcba48822d79315b508010e326747a7a9946b
Rigoberto Xocoxic <vacho19@gmail.com>
2026-03-21 07:47:31 -0600

    se instalo express usando el comando ** npm i express ** y se configuro para levantar el servidor en el puerto 4000 de local host en ubuntu

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,18 @@
-console.log('desde index .js y ya no dice que es experimental')
-console.log(2+80)
+const express = require('express')
+
+// Configurar la pp
+const app = express()
+
+// Definir una ruta
+app.get('/', (req, res)=>{
+    res.send('Hola, estoy desde http://localhost:4000/ en ubuntu')
+})
+
+
+// Definir puerto
+const PORT = process.env.PORT || 4000
+
+// Arrancar la app
+app.listen(PORT, () =>{
+    console.log('El servidor se esta ejecutando en el puerto:', PORT)
+})
