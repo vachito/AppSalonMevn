6605bd0b212a7a4a5bd1a91e14b1780a0d5f2764
Rigoberto Xocoxic <vacho19@gmail.com>
2026-03-21 09:58:58 -0600

    se crea un archivo separado para crear las rutas usando el router de express que ya viene incorporado y se agrega un archivo json con data statica

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,12 @@
 import express from 'express'
+import servicesRoutes from './routes/servicesRoutes.js'
 
 // Configurar la pp
 const app = express()
 
 // Definir una ruta usando req=request y res=response
 // enviar respuestas tipo json
-
-app.get('/', (req, res)=>{
-    const product = [
-        {
-            id:1,
-            price:30,
-            name:'laptop'
-        },
-        {
-            id:2,
-            price:50,
-            name:'monitor'
-        },
-    ]
-    res.json(product)
-})
+app.use('/api/services', servicesRoutes)
 
 
 // Definir puerto
