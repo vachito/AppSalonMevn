791ea9485ac68f9b238868abfeed02e06dcf9a1e
Rigoberto Xocoxic <vacho19@gmail.com>
2026-03-30 09:16:00 -0600

    validar que no existan usuarios duplicados, hashear el password con brcrypt

--- backend/Models/User.js
+++ backend/Models/User.js
@@ -1,4 +1,5 @@
 import mongosse from "mongoose";
+import bcrypt from 'bcrypt'
 import { uniqueId } from "../utils/index.js";
 
 const userSchema = mongosse.Schema({
@@ -17,6 +18,7 @@ const userSchema = mongosse.Schema({
     required: true,
     trim: true,
     unique: true,
+    lowercase:true
   },
   token: {
     type: String,
@@ -32,6 +34,13 @@ const userSchema = mongosse.Schema({
   },
 })
 
+userSchema.pre('save', async function (next){
+  if(!this.isModified('password')){
+    next()
+  }
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password,salt)
+})
 const User = mongosse.model('User', userSchema)
 
 export default User
--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,20 +1,32 @@
 import User from "../Models/User.js"
 
 const register = async (req,res) =>{
+    //Valida todos los campos
     if(Object.values(req.body).includes('')){
         const error = new Error('Todos los campos son obligatorios')
+        return res.status(400).json({msg: error.message})
+    }
 
-        return res.status(400).json({
-            msg: error.message
-        })
+    const {email, password, name} = req.body
+
+    // Evitar registros duplicados
+    const userExists = await User.findOne({email})
+    if(userExists){
+        const error = new Error('El usuario ya se encuentra registrado')
+        return res.status(400).json({msg: error.message})
+    }
+
+    // Validar la extension de password
+    const MIN_PASSWORD_LENGTH = 8
+    if(password.trim().length < MIN_PASSWORD_LENGTH){
+        const error = new Error(`El password debe tener como minimo ${MIN_PASSWORD_LENGTH} caracteres`)
+        return res.status(400).json({msg: error.message})
     }
 
     try {
         const user = new User(req.body)
         await user.save()
-        res.json({
-            msg: 'El usuario se creo, correctamente, revita tu email'
-        })
+        res.json({msg: 'El usuario se creo, correctamente, revita tu email'})
     } catch (error) {
         console.log(error)
     }
