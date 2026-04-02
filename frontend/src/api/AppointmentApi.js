38a2f60cfa3fbd259db832369fae59bc167651d3
Rigoberto Xocoxic <vacho19@gmail.com>
2026-04-02 11:27:37 -0600

    creando un state con las citas del usuario autenticado

--- frontend/src/api/AppointmentApi.js
+++ frontend/src/api/AppointmentApi.js
@@ -6,5 +6,8 @@ export default {
     },
     getByDate(date){
         return api.get(`/appointments?date=${date}`)
-    },   
+    },
+    getUserAppointments(userId){
+        return api.get(`/users/${userId}/appointments`)
+    }   
 }
--- frontend/src/stores/user.js
+++ frontend/src/stores/user.js
@@ -1,21 +1,28 @@
 import { ref, onMounted, computed } from "vue";
 import { defineStore } from "pinia";
 import { useRouter } from "vue-router";
-
+import AppointmentApi from "@/api/AppointmentApi";
 import AuthApi from "@/api/AuthApi";
 
 export const useUserStore = defineStore('user', ()=>{
     const router = useRouter()
     const user=ref({})
+    const userAppointments = ref([])
 
     onMounted(async ()=>{
         try {
             const {data: {user: userData} } = await AuthApi.auth()
             user.value=userData 
+            await getUserAppointments()
         } catch (error) {
             console.log(error)
         }
     })
+
+    async function getUserAppointments() {
+        const {data} = await AppointmentApi.getUserAppointments(user.value._id) 
+        userAppointments.value = data
+    }
 
     function logout(){
         localStorage.removeItem('auth_token')
@@ -24,10 +31,13 @@ export const useUserStore = defineStore('user', ()=>{
     }
 
     const getUserName = computed(() => user.value?.name ? user.value?.name : '')
+    const noAppointments = computed(()=> userAppointments.value.length === 0)
 
     return{
         user,
+        userAppointments,
         logout,
-        getUserName
+        getUserName,
+        noAppointments
     }
 })
