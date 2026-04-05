030c5dbe8ea5692e435a327536a098bfe52902c8
Rigoberto Xocoxic <vacho19@gmail.com>
2026-04-05 09:28:23 -0600

    video 529 obtener las citas si el usuario es admin

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -10,12 +10,12 @@ const getUserAppointments = async (req, res) => {
     }
 
     try {
-        const appointments = await Appointment.find({
-            user,
-            date:{
-                $gte : new Date()
-            }
-        }).populate('services').sort({date:'asc'})
+        const query = req.user.admin ?
+            {date:{$gte : new Date()}} :
+            {user, date:{ $gte : new Date()}}
+
+        const appointments = await Appointment.find(query)
+            .populate('services').sort({date:'asc'})
 
         res.json(appointments) 
     } catch (error) {
